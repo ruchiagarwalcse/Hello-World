25141228bed8e6b275153e7e2692583f3419acfb
Ruchi Agarwal <ruchi.agarwalcse@gmail.com>
2015-02-10 01:10:08 -0800

    Initial commit

---
+++ src/main/scala/hello/HelloConfig.scala
@@ -0,0 +1,30 @@
+package hello
+
+import org.springframework.context.annotation.{Configuration, ComponentScan}
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+
+/**
+ * This object bootstraps Spring Boot web application.
+ * Via Gradle: gradle bootRun
+ *
+ * @author Ruchi Agarwal
+ * @since 1.0
+ */
+
+@Controller
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+class HelloConfig {
+
+  @RequestMapping(Array("/"))
+  @ResponseBody
+  def myApp(): String = {
+    "Hello World!! This is my first App."
+  }
+
+}
+
