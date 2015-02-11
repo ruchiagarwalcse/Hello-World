867efdf4c66899bf0b143ce4489aee9dbbde421c
RUCHI AGARWAL <ruchi.agarwalcse@gmail.com>
2015-02-11 15:58:02 -0800

    Update HelloConfig.scala

--- src/main/scala/hello/HelloConfig.scala
+++ src/main/scala/hello/HelloConfig.scala
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * This object bootstraps Spring Boot web application.
@@ -20,7 +21,7 @@ import org.springframework.stereotype.Controller;
 @ComponentScan
 class HelloConfig {
 
-  @RequestMapping(Array("/"))
+  @RequestMapping(value=Array("/"), method=Array(RequestMethod.GET))
   @ResponseBody
   def myApp(): String = {
     "Hello World!! This is my first App."
